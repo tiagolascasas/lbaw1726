f3a5b4d75bef76796191e45b7102c62487f9a8be
r_tor <r_torres96@live.com.pt>
2018-04-17 22:15:07 +0100

    try nº2

--- development-env/app/Http/Controllers/Auth/RegisterController.php
+++ development-env/app/Http/Controllers/Auth/RegisterController.php
@@ -19,7 +19,7 @@ class RegisterController extends Controller
     | provide this functionality without requiring any additional code.
     |
     */
-    use Validator,RegistersUsers;
+    use RegistersUsers;
 
     /**
      * Where to redirect users after registration.
