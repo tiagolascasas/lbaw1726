06bf1aee2422eabd5b1945ab2bb73f203a7c136c
r_tor <r_torres96@live.com.pt>
2018-04-23 16:43:21 +0100

    fixing home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,29 +7,29 @@
 @endphp
 
 @section('content')
-    <div class="content p-4 mt-3">
-        <main>
-            <section class="jumbotron text-center">
-                <div class="container">
-                    <h1 class="jumbotron-heading"><b>Welcome to Bookhub!</b></h1>
-                    <h4 class="text-dark hidden-p-md-down">Introduced in May of 2018, BookHub is a web platform that allows EU residents to participate in online book auctions.</h4>
-                    @if (Auth::check())
-                    <span id="buttonsWelcome">
-                        <a href="myAuctions.html" class="btn btn-primary btn-lg my-2 mx-3 jumbotron-buttons">My Auctions</a>
-                        <a href="auctionsIm_in.html" class="btn btn-secondary btn-lg my-2 mx-3 jumbotron-buttons">"Auctions I'm in"</a>
-                    </span>
-                    @endif
-                </div>
-            </section>
 
+<div class="content p-4 mt-3">
+    <main>
+        <section class="jumbotron text-center">
+            <div class="container">
+                <h1 class="jumbotron-heading"><b>Welcome to Bookhub!</b></h1>
+                <h4 class="text-dark hidden-p-md-down">Introduced in May of 2018, BookHub is a web platform that allows EU residents to participate in online book auctions.</h4>
+                @if (Auth::check())
+                <span id="buttonsWelcome">
+                    <a href="myAuctions.html" class="btn btn-primary btn-lg my-2 mx-3 jumbotron-buttons">My Auctions</a>
+                    <a href="auctionsIm_in.html" class="btn btn-secondary btn-lg my-2 mx-3 jumbotron-buttons">"Auctions I'm in"</a>
+                </span>
+                 @endif
+            </div>
+        </section>
 <!---  Active auctions grid -->
-<div class="album py-2">
-    <div class="row">
-      <!--- Items list -->
-      @include('pages.auctionItems')
-    </div>
-</div>
-</main>
+        <div class="album py-2">
+            <div class="row">
+             <!--- Items list -->
+            @include('pages.auctionItems')
+            </div>
+        </div>
+    </main>
 </div>
 
 @endsection
