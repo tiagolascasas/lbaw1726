f090b44f634f1c83337b8751bb3126688482a6c3
3rdvision <kazgru@gmail.com>
2018-04-25 14:37:35 +0100

    small fix

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -14,8 +14,8 @@
                 <h4 class="text-dark hidden-p-md-down">Introduced in May of 2018, BookHub is a web platform that allows EU residents to participate in online book auctions.</h4>
                 @if (Auth::check())
                 <span id="buttonsWelcome">
-                    <a href="myAuctions.html" class="btn btn-primary btn-lg my-2 mx-3 jumbotron-buttons">My Auctions</a>
-                    <a href="auctionsIm_in.html" class="btn btn-secondary btn-lg my-2 mx-3 jumbotron-buttons">"Auctions I'm in"</a>
+                    <a href="{{ url('myauctions/')}}" class="btn btn-primary btn-lg my-2 mx-3 jumbotron-buttons">My Auctions</a>
+                    <a href="{{ url('auctionsim_in/')}}" class="btn btn-secondary btn-lg my-2 mx-3 jumbotron-buttons">Auctions I'm in</a>
                 </span>
                  @endif
             </div>
