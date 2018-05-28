83e9994366ffdfe381dc92f7562cb04a6a81cc2e
3rdvision <kazgru@gmail.com>
2018-05-28 21:18:12 +0100

    frontend for retrieving password

---
+++ development-env/resources/views/auth/forgot_password.blade.php
@@ -0,0 +1,15 @@
+
+<form method="POST" action="{{ route('login') }}">
+    {{ csrf_field() }}
+    <div class="modal-body">
+        <div class="form-group">
+            <label for="email">E-mail</label>
+        <input class="form-control" id="email" name="email" type="email" placeholder="Your E-mail adress"  value="{{ old('email') }}" required>
+        </div>
+    </div>
+    <div class="modal-footer">
+        <button class="btn btn-primary btn-block" type="submit">
+            RETRIEVE PASSWORD
+        </button>
+    </div>
+</form>
